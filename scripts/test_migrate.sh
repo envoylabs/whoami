e97d7b1a43661390eb58b6dd78afc1a1ef4e9e6a
Alex Lynham <alex@lynh.am>
2022-05-18 11:37:22 +0100

    Update juno version

--- scripts/test_migrate.sh
+++ scripts/test_migrate.sh
@@ -7,7 +7,7 @@ then
 fi
 
 # pinched and adapted from DA0DA0
-IMAGE_TAG=${2:-"v4.0.0"}
+IMAGE_TAG=${2:-"v6.0.0-alpha"}
 CONTAINER_NAME="juno_whoami"
 BINARY="docker exec -i $CONTAINER_NAME junod"
 DENOM='ujunox'
